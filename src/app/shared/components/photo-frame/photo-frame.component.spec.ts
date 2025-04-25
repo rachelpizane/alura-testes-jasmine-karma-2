9d31a650f58cbff885ccd8ebd5fca9615850d77d
Rachel Pizane <rachel.maia@dbserver.com.br>
2025-04-25 17:16:02 -0300

    test: criado testes com fakeAsync e tick()

--- src/app/shared/components/photo-frame/photo-frame.component.spec.ts
+++ src/app/shared/components/photo-frame/photo-frame.component.spec.ts
@@ -1,4 +1,4 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
 
 import { PhotoFrameComponent } from './photo-frame.component';
 
@@ -22,4 +22,33 @@ describe('PhotoFrameComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it(`#${PhotoFrameComponent.prototype.like.name} should trigger (@Output liked) once when called multiple times within debounce time`, fakeAsync(() => {
+    let times = 0
+    component.liked.subscribe(() => {
+      times++;
+    });
+
+    component.like();
+    component.like();
+    tick(500) // Espera 500ms para que o debounce seja acionado
+
+    expect(times).toBe(1);
+  }));
+
+  it(`#${PhotoFrameComponent.prototype.like.name} should trigger (@Output liked) two times when called outside debounce time`, fakeAsync(() => {
+    const DEBOUNCE_TIME: number = 500;
+    let times: number = 0
+
+    component.liked.subscribe(() => {
+      times++;
+    });
+
+    component.like();
+    tick(DEBOUNCE_TIME)
+    component.like();
+    tick(DEBOUNCE_TIME)
+
+    expect(times).toBe(2);
+  }));
 });
