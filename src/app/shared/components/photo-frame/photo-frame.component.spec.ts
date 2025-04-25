d1d361abcdec4001ee8326e4f2651ed23218b465
Rachel Pizane <rachel.maia@dbserver.com.br>
2025-04-25 18:06:11 -0300

    test: incluido um teste que verifica a acessibilidade pelo .getAttribute()

--- src/app/shared/components/photo-frame/photo-frame.component.spec.ts
+++ src/app/shared/components/photo-frame/photo-frame.component.spec.ts
@@ -58,7 +58,7 @@ describe('PhotoFrameComponent', () => {
   it(`Should display number of likes when (@Input likes) is incremented`, () => {
     component.likes++;
     fixture.detectChanges(); // Atualiza a visualização após a mudança de valor
-    
+
     const element: HTMLElement = fixture.nativeElement.querySelector('.like-counter');
     // O fixture.nativeElement é o elemento DOM do componente
     // O querySelector é usado para selecionar o elemento DOM que contém o número de likes
@@ -67,5 +67,21 @@ describe('PhotoFrameComponent', () => {
     expect(element.textContent.trim()).toBe('1');
     // Verifica se o texto do elemento DOM é igual a 1
     // O trim() é usado para remover espaços em branco no início e no final do texto. Sem isso, o teste falharia. (Error: Expected ' 1 ' to be '1')
+  });
+
+  it(`Should update aria-label when (@Input likes is incremented)`, () => {
+    component.likes++;
+    fixture.detectChanges();
+
+    const element: HTMLElement = fixture.nativeElement.querySelector('.like-counter');
+
+    expect(element.getAttribute('aria-label')).toBe('1: people liked');
+    // .getAttribute() é usado para obter o valor de um atributo no elemento DOM. Nessa caso, estamos obtendo o valor do atributo aria-label
+  });
+
+  it(`Should have aria-label with default (@Input likes) value (0)`, () => {
+    const element: HTMLElement = fixture.nativeElement.querySelector('.like-counter');
+
+    expect(element.getAttribute('aria-label')).toBe('0: people liked');
   });
 });
