4b39ed919625b0b94ccc61bda853d4ff081a649d
Rachel Pizane <rachel.maia@dbserver.com.br>
2025-04-28 14:43:29 -0300

    test: incluido testes com componente fantoche e utilizando o debugElement

--- src/app/directives/action.directive.spec.ts
+++ src/app/directives/action.directive.spec.ts
@@ -1,8 +1,51 @@
+import { Action } from 'rxjs/internal/scheduler/Action';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ActionDirective } from './action.directive';
+import { ActionDirectiveModule } from './action.module';
+import { Component } from '@angular/core';
+import { By } from '@angular/platform-browser';
 
-describe('ActionDirective', () => {
-  it('should create an instance', () => {
-    const directive = new ActionDirective();
-    expect(directive).toBeTruthy();
-  });
+describe(ActionDirective.name, () => {
+  let fixture: ComponentFixture<ActionDirectiveTestComponent> = null;
+  let component: ActionDirectiveTestComponent = null;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ActionDirectiveTestComponent],
+      imports: [ActionDirectiveModule]
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(ActionDirectiveTestComponent);
+    component = fixture.componentInstance;
+  })
+
+  it(`(D) (@Output appAction) should emit event with payload when ENTER key is pressed`, () => {
+    const divEl: HTMLElement = fixture.debugElement.query(By.directive(ActionDirective)).nativeElement
+    const event: KeyboardEvent = new KeyboardEvent('keyup', { key: 'Enter' });
+    divEl.dispatchEvent(event)
+
+    expect(component.hasEvent()).toBeTrue
+  })
+
+  it(`(D) (@Output appAction) should emit event with payload when clicked`, () => {
+    const divEl: HTMLElement = fixture.nativeElement.querySelector('.dummy-component');
+    divEl.click()
+
+    expect(component.hasEvent()).toBeTrue;
+  })
+
 });
+@Component({
+  template: `<div class="dummy-component" (appAction)="actionHandler($event)"></div>`
+})
+class ActionDirectiveTestComponent {
+  private event!: Event;
+
+  actionHandler(event: Event): void {
+    this.event = event;
+  }
+
+  hasEvent(): boolean {
+    return !!this.event;
+  }
+}
